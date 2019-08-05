f9a0b37138862c9c0509dbcb3b971a923306a8a3
Jonathan Velez <mrvelez13@gmail.com>
2019-08-05 14:47:56 -0500

    Construcción de controller y archivo con datos de prueba

--- crud-basico-2/src/main/java/com/accenture/api/controller/ClienteRestController.java
+++ crud-basico-2/src/main/java/com/accenture/api/controller/ClienteRestController.java
@@ -1,5 +1,58 @@
 package com.accenture.api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.accenture.api.entity.Cliente;
+import com.accenture.api.service.IClienteService;
+
+@RestController
+@RequestMapping("/api")
 public class ClienteRestController {
+	@Autowired
+	private IClienteService clienteService;
+	
+	@GetMapping("/clientes")
+	public List<Cliente> listar(){
+		return clienteService.findAll();
+	}
+	
+	@PostMapping("/clientes")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Cliente create(@RequestBody Cliente cliente) {
+		return clienteService.save(cliente);
+	}
+	
+	@GetMapping("/clientes/{id}")
+	public Cliente show(@PathVariable Long id) {
+		return clienteService.findById(id);
+	}
+	
+	@PutMapping("/clientes")
+	@ResponseStatus( code = HttpStatus.CREATED)
+	public Cliente update(@RequestBody Cliente cliente) {
+		Cliente clienteToUpdate = clienteService.findById(cliente.getId());
+		clienteToUpdate.setNombre( cliente.getNombre() );
+		clienteToUpdate.setApellido( cliente.getApellido() );
+		clienteToUpdate.setEmail( cliente.getEmail() );
+		
+		return clienteService.save(clienteToUpdate);		
+	}
+	
+	@DeleteMapping("/clientes/{id}")
+	public void delete(@PathVariable Long id) {
+		clienteService.delete(id);		
+	}
 
 }
---
+++ crud-basico-2/src/main/resources/import.sql
@@ -0,0 +1,11 @@
+/* Populate tabla clientes */
+INSERT INTO clientes (nombre, apellido, email, fecha_creacion) VALUES('Hug', 'Jackman', 'wolverine@accenture.com', '2019-08-02');
+INSERT INTO clientes (nombre, apellido, email, fecha_creacion) VALUES('Charlize', 'Theron', 'aeonflux@accenture.com', '2019-08-02');
+INSERT INTO clientes (nombre, apellido, email, fecha_creacion) VALUES('Robert', 'Downey', 'ironman@accenture.com', '2019-08-02');
+INSERT INTO clientes (nombre, apellido, email, fecha_creacion) VALUES('Scarlett', 'Johansson', 'viudanegra@accenture.com', '2019-08-02');
+INSERT INTO clientes (nombre, apellido, email, fecha_creacion) VALUES('Halle', 'Berry', 'tormenta@accenture.com', '2019-08-02');
+INSERT INTO clientes (nombre, apellido, email, fecha_creacion) VALUES('Gal', 'Gadot', 'wonderwoman@accenture.com', '2019-08-02');
+INSERT INTO clientes (nombre, apellido, email, fecha_creacion) VALUES('Milla', 'Jovovich', 'alice@accenture.com', '2019-08-02');
+INSERT INTO clientes (nombre, apellido, email, fecha_creacion) VALUES('Elizabeth', 'Olsen', 'brujaescarlata@accenture.com', '2019-08-02');
+INSERT INTO clientes (nombre, apellido, email, fecha_creacion) VALUES('Karen', 'Guillan', 'nebula@accenture.com', '2019-08-02');
+INSERT INTO clientes (nombre, apellido, email, fecha_creacion) VALUES('Zoe', 'Zaldana', 'gamora@accenture.com', '2019-08-02');
